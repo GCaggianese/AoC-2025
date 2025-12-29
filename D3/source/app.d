404352d0c3f9ef35e4d028cbffd6fb7fca2e0c16
GCaggianese <german.caggianese@pm.me>
2025-12-29 20:12:52 -0300

    D3 +1 🌟

--- D3/source/app.d
+++ D3/source/app.d
@@ -3,8 +3,43 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import std.stdio;
+import std.algorithm;
+import std.range;
+import std.conv;
+
+int jolts = 0;
+
+void outputJoltage(int[] og){
+    int[] aux1 = og.dropBack(1);
+    auto maxIndex = aux1.maxIndex();
+
+    jolts += aux1.maxElement()*10;
+
+    int[] aux2 = og.drop(maxIndex + 1);
+    jolts += aux2.maxElement();
+    writeln("Plus: ", aux1.maxElement()*10 + aux2.maxElement());
+    writeln("Jolts = ", jolts, "\n");
+}
+
+int[] parseLine(string input){
+    int[] ret;
+    string[] aux = input.split("");
+    foreach (i;aux){
+        ret ~= parse!int(i);
+    }
+    writeln(ret);
+    return ret;
+}
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+
+    auto file = File("input.txt", "r");
+    foreach (line; file.byLine()) {
+        outputJoltage(parseLine(line.idup));
+    }
+    file.close();
+
+    writeln("--------- p1 result ---------");
+    writeln("Jolts: ", jolts);
 }
