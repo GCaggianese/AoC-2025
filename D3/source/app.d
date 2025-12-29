e5db1d1212bebc09202743b1d5ac6703a84579b3
GCaggianese <german.caggianese@pm.me>
2025-12-29 00:19:02 -0300

    D3 Init

---
+++ D3/source/app.d
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2025 Germán Caggianese <german.caggianese@pm.me>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+import std.stdio;
+
+void main()
+{
+	writeln("Edit source/app.d to start your project.");
+}
--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Solutions to the 2025 puzzles!
 
 1. [Day 1 - C++](./D1/README.org)
 2. [Day 2 - C](./D2/README.md)
+2. [Day 3 - D](./D3/README.org)
