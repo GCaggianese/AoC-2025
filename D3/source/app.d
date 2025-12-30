062c2aca039d38fac6c2daf92559c55f1759aa83
GCaggianese <german.caggianese@pm.me>
2025-12-29 23:55:36 -0300

    D3 p2 🌟

--- D3/source/app.d
+++ D3/source/app.d
@@ -6,8 +6,13 @@ import std.stdio;
 import std.algorithm;
 import std.range;
 import std.conv;
+import std.math : pow;
+import std.algorithm : maxElement, countUntil;
+import std.array : array;
+
 
 int jolts = 0;
+long jolts_p2 = 0;
 
 void outputJoltage(int[] og){
     int[] aux1 = og.dropBack(1);
@@ -21,11 +26,42 @@ void outputJoltage(int[] og){
     writeln("Jolts = ", jolts, "\n");
 }
 
+
+void outputJoltage_p2(long[] og){
+    long multiplier = cast(long) pow(10.0, 11);
+    string result = "";
+
+    for(int i = 0; i < 12; i++){
+        int remaining_needed = 12 - i;
+        auto window_size = og.length - remaining_needed + 1;
+        long[] window = og[0..window_size].array;
+        long maxVal = window.maxElement;
+        auto maxIdx = window.countUntil(maxVal);
+        result ~= to!string(maxVal);
+        jolts_p2 += maxVal * multiplier;
+        og = og[maxIdx + 1 .. $];
+        multiplier /= 10;
+    }
+
+    writeln("Plus: ", result);
+    writeln("Jolts = ", jolts_p2);
+}
+
 int[] parseLine(string input){
     int[] ret;
     string[] aux = input.split("");
     foreach (i;aux){
         ret ~= parse!int(i);
+    }
+    writeln(ret);
+    return ret;
+}
+
+long[] parseLine_p2(string input){
+    long[] ret;
+    string[] aux = input.split("");
+    foreach (i;aux){
+        ret ~= parse!long(i);
     }
     writeln(ret);
     return ret;
@@ -41,5 +77,15 @@ void main()
     file.close();
 
     writeln("--------- p1 result ---------");
-    writeln("Jolts: ", jolts);
+    writeln("Jolts: ", jolts, "\n\n");
+
+    auto file_p2 = File("input_2.txt", "r");
+    foreach (line; file_p2.byLine()) {
+        outputJoltage_p2(parseLine_p2(line.idup));
+    }
+    file_p2.close();
+
+    writeln("--------- p2 result ---------");
+    writeln("Jolts p2: ", jolts_p2);
+
 }
